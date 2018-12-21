f293aa45d679a6fd8aba114533afbdde94c780ea
Adam Cooper <amcooper@gmail.com>
2018-12-20 22:47:30 -0500

    wip

--- graphql/schema.js
+++ graphql/schema.js
@@ -41,6 +41,120 @@ const { nodeInterface, nodeField } = nodeDefinitions(
   }
 );
 
+
+const articleType = new GraphQLObjectType({
+  name: 'Article',
+  description: 'Article',
+  interfaces: [ nodeInterface ],
+  fields: () => ({
+    id: globalIdField(),
+    headline: {
+      type: GraphQLString,
+      description: 'Article headline',
+    },
+    subhed: {
+      type: GraphQLString,
+      description: 'Arricle subhed',
+    },
+    excerpt: {
+      type: GraphQLString,
+      description: 'Arricle excerpt for the promo',
+    },
+    body: {
+      type: GraphQLString,
+      description: 'Article body'
+    },
+    authors: {
+      type: authorConnection,
+      description: 'Article authors',
+      args: connectionArgs,
+      // confirm this resolver- it looks off
+      resolve: (author, args) => connectionFromArray(author.articles.map(getAuthor), args)
+    }
+  })
+});
+
+
+/*
+ * Old schema
+ * 
+module.exports = `
+  # scalar DateTime
+
+  enum PublicationStatus {
+    DRAFT
+    PUBLISHED
+  }
+
+  type Article {
+    id: ID!
+    headline: String!
+    subhed: String
+    excerpt: String
+    authors: [Author]
+    tags: [Tag]
+    image_url: String
+    body: String!
+    publication_time: String
+    publication_status: PublicationStatus
+    created_at: String
+    updated_at: String
+  }
+
+  type Author {
+    id: ID!
+    name: String!
+    sort_name: String
+    email: String
+    articles: [Article]
+    created_at: String
+    updated_at: String
+  }
+
+  type Tag {
+    id: ID!
+    name: String!
+    description: String
+    articles: [Article]
+    created_at: String
+    updated_at: String
+  }
+
+  type Link {
+    id: ID!
+    name: String!
+    description: String
+    url: String!
+    category: Category 
+    created_at: String
+    updated_at: String
+  }
+
+  type Category {
+    id: ID!
+    name: String!
+    description: String
+    links: [Link]
+    created_at: String
+    updated_at: String
+  }
+
+  type Page {
+    id: ID!
+    title: String!
+    subtitle: String
+    body: String
+    publication_time: String
+    publication_status: PublicationStatus
+    created_at: String
+    updated_at: String
+  }
+
+  type Query {
+    feed(last: Int): [Article]
+  }
+`
+*/
 /*
  * New Relay-compatible schema
  * 
