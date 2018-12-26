5c2d6b0c81043cd41f21ecf44c02c6a8d598dfba
Adam Cooper <amcooper@gmail.com>
2018-12-25 19:14:38 -0500

    wip

--- models/Tag.js
+++ models/Tag.js
@@ -2,6 +2,8 @@ const knex = require("../config/database.js");
 
 const getTagsByArticle = articleId => knex
   .from("tags")
-  .innerJoin("articles_tags", "articles_tags.article_id", articleId);
+  .innerJoin("articles_tags", function() {
+    this.on("articles_tags.article_id", "=", articleId).andOn("articles_tags.tag_id", "=", "tags.id")
+  });
 
 module.exports = getTagsByArticle;
