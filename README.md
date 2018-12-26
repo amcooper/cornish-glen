b4b61ada9d71fa5bb3f9112dd690a28e9ff3589a
Adam Cooper <theadamcooper@protonmail.com>
2018-12-26 14:32:42 -0500

    uh oh added pwd to db for Linux Mint and also README

---
+++ README.md
@@ -0,0 +1,4 @@
+### Cornish Glen
+##### GraphQL API for renowned periodical _Turnip Glen_
+
+N.B. Due to development on three different operating systems, there may be some weirdness with the database credentials. PostgreSQL/Debian needs passwords where other OS do not. Or something. So we may need to move development to SQLite.
--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,8 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: "postgres://localhost:5432/cornish_glen",
+    connection: "postgres://adam:Zahirie6@localhost:5432/cornish_glen",
+    // connection: { user: "adam", database: "cornish_glen", password: "Zahirie6" },
     pool: {
       min: 2,
       max: 10
