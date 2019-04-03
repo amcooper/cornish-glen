83f5b94303246e1e04659ab7269c1936c1fab3ac
Adam Cooper <amcooper@gmail.com>
2019-04-03 14:47:43 -0400

    Serialized comment ids

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,11 @@
 const knex = require("../config/database.js");
 
+const base64decode = serializedId => {
+	const [tableName, sqlId] = Buffer.from(serializedId, "base64").to("ascii").split(":");
+	console.log("\n[DEBUG][cornish-glen] ", serializedId, tableName, sqlId);
+	return sqlId;
+}
+
 const getComment = id => {
 	return knex("comments").where("id", id);
 }
@@ -11,9 +17,9 @@ const getCommentsQty = () => knex("comments").count("id");
 const addComment = ({ body, parentCommentId = undefined, articleId, authorId = 9 }) => knex("comments")
 	.insert({
 		body,
-		parent_comment_id: parentCommentId,
-		article_id: articleId,
-		author_id: authorId,
+		parent_comment_id: base64decode(parentCommentId),
+		article_id: base64decode( articleId ),
+		author_id: base64decode( authorId ),
 		publication_time: new Date(Date.now()),
 		created_at: new Date(Date.now()),
 		updated_at: new Date(Date.now())
