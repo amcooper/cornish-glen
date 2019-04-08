a673c54df28f5521ff8706f7cd098fc310886a7d
Adam Cooper <amcooper@gmail.com>
2019-04-08 12:25:20 -0400

    Fixed base64decode to ignore falsey values

--- models/Comment.js
+++ models/Comment.js
@@ -1,9 +1,13 @@
 const knex = require("../config/database.js");
 
 const base64decode = serializedId => {
-	const [tableName, sqlId] = Buffer.from(serializedId, "base64").to("ascii").split(":");
-	console.log("\n[DEBUG][cornish-glen] ", serializedId, tableName, sqlId);
-	return sqlId;
+	if (!serializedId) {
+		return serializedId
+	} else {
+		const [tableName, sqlId] = Buffer.from(serializedId, "base64").toString("ascii").split(":");
+		console.log("\n[DEBUG][cornish-glen] ", serializedId, tableName, sqlId);
+		return sqlId;
+	}
 }
 
 const getComment = id => {
@@ -14,16 +18,19 @@ const getCommentsByArticle = articleId => knex("comments").where("article_id", a
 
 const getCommentsQty = () => knex("comments").count("id");
 
-const addComment = ({ body, parentCommentId = undefined, articleId, authorId = 9 }) => knex("comments")
-	.insert({
-		body,
-		parent_comment_id: base64decode(parentCommentId),
-		article_id: base64decode( articleId ),
-		author_id: base64decode( authorId ),
-		publication_time: new Date(Date.now()),
-		created_at: new Date(Date.now()),
-		updated_at: new Date(Date.now())
-	});
+const addComment = ({ body, parentCommentId = undefined, articleId, authorId = 9 }) => {
+	debugger;
+	return knex("comments")
+		.insert({
+			body,
+			parent_comment_id: base64decode(parentCommentId),
+			article_id: base64decode( articleId ),
+			author_id: base64decode( authorId ),
+			publication_time: new Date(Date.now()),
+			created_at: new Date(Date.now()),
+			updated_at: new Date(Date.now())
+		});
+}
 
 module.exports = {
 	getComment,
